47ea5d59bf953e780a9c17c09ba2e4015c2146af
Ashwin Balasubramanian <balathinnappan@gmail.com>
2018-04-10 18:33:25 -0500

    KMP documentation added

--- src/KMP.java
+++ src/KMP.java
@@ -1,60 +1,89 @@
 import java.util.HashMap ;
 
-public class KMP {
+class KMP {
     private String text, pattern ;
-    private HashMap<Integer, Integer> auxillary ;
+    private HashMap<Integer, Integer> auxiliary;
+
+    /**
+     *
+     * Default Constructor. Builds the pre-processed table.
+     *
+     * @param text Corpora in which the pattern is to be found.
+     * @param pattern Pattern which is to be found in the corpora.
+     */
     KMP(String text, String pattern){
         this.text = text ;
         this.pattern = pattern ;
-        System.out.println(pattern.length());
-        auxillary = pre_processing(pattern) ;
+        auxiliary = pre_processing(pattern) ;
         search();
     }
 
-    void search(){
-        int index = 0, pointer = 0 ;
+    /**
+     *
+     * Search for the given pattern in the given corpora.
+     * If pattern is found. It will return its index.
+     *
+     */
+    private void search(){
+        int index = 0, pointer = 0, found = 0 ;
         while ( index < text.length()) {
             if( pattern.charAt(pointer) == text.charAt(index)) {
+                /*
+                If the current character matches a character in the pattern.
+                 */
                 index++;
                 pointer++;
             }
             if( pointer == pattern.length()){
-                System.out.println("Found Pattern at index"+(index-pointer)) ;
-                pointer = auxillary.get(pointer-1) ;
+                /*
+                If a pattern has been found.
+                 */
+                found++ ;
+                System.out.println("Found Pattern at index: "+(index-pointer)) ;
+                pointer = auxiliary.get(pointer-1) ;
             }
-            else if(index < text.length() && pattern.charAt(pointer) != text.charAt(index)){
+            else if( index < text.length() && (pattern.charAt(pointer) != text.charAt(index))){
                 /*
                  * Sliding the window.
                  */
                 if( pointer != 0)
-                    pointer = auxillary.get(pointer-1) ;
+                    pointer = auxiliary.get(pointer-1) ;
                 else
                     index++ ;
             }
         }
-
+        if ( found ==0 )
+            System.out.println("Pattern cannot be found.");
     }
+
+    /**
+     *
+     * The failure function in Knuth-Morris-Pratt algorithm. To not match a single character more than once.
+     *
+     * @param pattern Given pattern to be matched.
+     * @return preprocessed table (array).
+     */
     private HashMap<Integer, Integer> pre_processing(String pattern){
         int value = 0 , key = 1;
-        HashMap<Integer, Integer> auxillary = new HashMap<>();
-        auxillary.put(0, 0) ;
+        HashMap<Integer, Integer> auxiliary = new HashMap<>();
+
+        auxiliary.put(0, 0) ;
+
         while(key < pattern.length()){
            if( pattern.charAt(key) == pattern.charAt(value) ){
                value++;
-               auxillary.put(key, value) ;
+               auxiliary.put(key, value) ;
                key++;
            }
            else{
                if(value != 0)
-                   value = auxillary.get((value - 1)) ;
+                   value = auxiliary.get((value - 1)) ;
                else{
-                   auxillary.put(key, value) ;
+                   auxiliary.put(key, value) ;
                    key++ ;
                }
            }
         }
-
-        return auxillary ;
+        return auxiliary ;
     }
-
 }

2909e30f71e87dd996ac95f137a7089b585394ac
Ashwin Balasubramanian <balathinnappan@gmail.com>
2018-04-10 18:32:16 -0500

    KMP documentation added

--- src/PlagiarismDetector.java
+++ src/PlagiarismDetector.java
@@ -1,8 +1,10 @@
 public class PlagiarismDetector {
     private static LeastCommonSubSequence LCSS = new LeastCommonSubSequence("Gautam", "Gutman") ;
     public static void main(String args[]){
-
         System.out.println(LCSS.getSequenceLength()) ;
         System.out.println(LCSS.getSequence()) ;
+        new KMP("AABAACAADAABAABA", "AABA") ;
+        new KMP("Gautam", "ut") ;
+
     }
 }
